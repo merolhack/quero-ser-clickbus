b4dd7fd7a45054e3f09d7eb5e7da25fbbdaadc45
Lenin Meza <lmezaz@mnyl.com.mx>
2015-10-08 18:10:16 -0500

    Validación del resto

--- testes/cash-machine/assets/ammount.js
+++ testes/cash-machine/assets/ammount.js
@@ -4,21 +4,26 @@ $(document).ready(function() {
 		var currency = $('#form_ammount input[name=monto]').val();
 		if ( currency.length > 0 ) {
 			var number = Number(currency.replace(/[^0-9\.]+/g,""));
-			if ( number >= 10 ) {
-				$.ajax({
-					url: 'ammount/'+number,
-					type: 'POST',
-					success: function ( dataset ) {
-						var obj = jQuery.parseJSON( dataset );
-						$("#page_description").empty();
-						$("#page_description").append("<h2>Tome sus billetes:</h2>");
-						$.each( obj, function( key, value ) {
-							$("#page_description").append("<p>"+value+"</p>");
-						});
-					}
-				});
+			var resto = number % 10;
+			if ( resto == 0 ) {
+				if ( number >= 10 ) {
+					$.ajax({
+						url: 'ammount/'+number,
+						type: 'POST',
+						success: function ( dataset ) {
+							var obj = jQuery.parseJSON( dataset );
+							$("#page_description").empty();
+							$("#page_description").append("<h2>Tome sus billetes:</h2>");
+							$.each( obj, function( key, value ) {
+								$("#page_description").append("<p>"+value+"</p>");
+							});
+						}
+					});
+				} else {
+					alert('Ingrese un monto mayor a $10');
+				}
 			} else {
-				alert('Ingrese un monto mayor a $10');
+				alert('El billete con menor denominación es $10');
 			}
 		} else {
 			alert('Ingrese un campo del tipo: $123,456.78');
